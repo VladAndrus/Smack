7d99bb527738cfcec295781cec6b9f9bedf5ccfc
Vlad <vladyslav.andrus@gmail.com>
2019-07-28 18:52:22 +0200

    add loginvc

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Влад on 7/28/19.
+//  Copyright © 2019 Vlad. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Влад on 7/28/19.
+//  Copyright © 2019 Vlad. All rights reserved.
+//
+
+import Foundation
+
+
+//Seques
+let TO_LOGIN = "toLogin"
